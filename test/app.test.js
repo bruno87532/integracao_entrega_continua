def5c98571ebc984f4ad5d07bb8cc484c59312df
BRUNO HENRIQUE GUINERIO <2901392323016@fatecararas.com>
2025-06-07 10:13:48 -0300

    [FEAT] initiall commit of sample application with test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const { getWelcomeMessage } = require('../src/app'); 
+
+ 
+
+// Teste unitário para a função getWelcomeMessage 
+
+test('getWelcomeMessage should return "Welcome to the CI/CD pipeline!"', () => { 
+
+  expect(getWelcomeMessage()).toBe('Welcome to the CI/CD pipeline!'); 
+
+}); 
